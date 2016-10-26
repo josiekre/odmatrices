3b6d94525b43e4ca6eff5be48067798f16a0dbf2
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-10-26 09:14:34 -0400

    Add zones to dummy data
    The two-zone deal wasn't working well with the IPF function.

--- data-raw/testdata.R
+++ data-raw/testdata.R
@@ -1,11 +1,16 @@
 library(tidyverse)
 
 # volume in a made-up OD matrix
-volumes <- c(15121, 127, 603, 213, 0, 614, 526, 712,0)
-zones <- c("I", "A", "B")   # internal, two external zones
+zones <- c("I1", "I2", "A", "B", "C")   # two internal, three external zones
+volumes <- c(
+  7834, 7625, 127, 603, 231,
+  6439, 8916, 913, 275, 108,
+  285, 827, 0, 519, 947,
+  816, 301, 371, 0, 821,
+  213, 341, 726, 712, 0)
 
 # matrix representation
-test_odmatrix <- matrix(volumes, byrow = TRUE, ncol = 3)
+test_odmatrix <- matrix(volumes, byrow = TRUE, ncol = 5)
 colnames(test_odmatrix) <- rownames(test_odmatrix) <- zones
 devtools::use_data(test_odmatrix, overwrite = TRUE)
 
@@ -19,7 +24,7 @@ test_oddf <- expand.grid(
 devtools::use_data(test_oddf, overwrite = TRUE)
 
 # counts
-test_counts <- data_frame(station = zones[-1], aawdt = c(1826, 3212))
+test_counts <- data_frame(station = zones[3:length(zones)], aawdt = c(4218, 3805, 5213))
 devtools::use_data(test_counts, overwrite = TRUE)
 
 
